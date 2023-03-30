9c7209207ccfb6e5650735b68a8eb4f85dc1be01
mericalp <83503845+mericalp@users.noreply.github.com>
2023-03-30 20:15:52 +0300

    QuizApp-testdriven

---
+++ QuizApp-TestDriven/QuizApp-TestDriven/QuizApp_TestDrivenApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizApp_TestDrivenApp.swift
+//  QuizApp-TestDriven
+//
+//  Created by Meric Alp on 30.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct QuizApp_TestDrivenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
